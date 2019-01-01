ae06e74debfca2f46ffafc8ac45d5ddaaf7ed143
Ashkan Ebrahimi <ashkan.webg@gmail.com>
2019-01-01 11:44:47 +0330

    Update Script

--- Installer.sh
+++ Installer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 clear
-echo "============================================"
-echo "WordPress Install Script"
-echo "============================================"
-echo "Database Name: "
-read -e dbname
-echo "Database User: "
-read -e dbuser
-echo "Database Password: "
-read -s dbpass
+echo "======================================================================================================"
+echo -e "WordPress Install Script"
+echo "Script By Ashkan Ebrahimi"
+echo "Script For Control Panel DirectAdmin"
+echo "Git: AshkanSecure | GitHub: AshkanSecure | Instagram: Secure_Manager | Email: ashkan.secure@gmail.com|"
+echo "======================================================================================================="
+read -p '1) Please Enter DataBase Name:' dbname; echo  " You'r DataBase Name Has $dbname "
+read -p '2) Please Enter DataBase User: ' dbuser; echo " You'r DataBase User Has $dbuser "
+read -p '3) Please Enter Password DataBase: ' dbpass ' echo " You'r DataBase Password $dbpass "
+read -p '4) Please Insert HostName: ' hostname ; echo " You'r Host Name Has $hostname "
 echo "run install? (y/n)"
 read -e run
 if [ "$run" == n ] ; then
@@ -18,7 +19,9 @@ echo "============================================"
 echo "A robot is now installing WordPress for you."
 echo "============================================"
 #download wordpress
-curl -O https://wordpress.org/latest.tar.gz
+wget https://wordpress.org/latest.tar.gz -p /home/$hostname/public_html/
+#move to Directory Hostname
+cd /home/$hostname/public_html
 #unzip wordpress
 tar -zxvf latest.tar.gz
 #change dir to wordpress
@@ -59,11 +62,16 @@ echo "Cleaning..."
 #remove zip file
 rm latest.tar.gz
 #Import Hostname
-echo "HostName: "
-read -e host_name
 chown $host_name:$hostname -R /home/$host_name/public_html/
 #
 echo "========================="
 echo "Installation is complete."
 echo "========================="
 fi
+echo "======================================================================================================"
+echo "WordPress Install Script"
+echo "Script By Ashkan Ebrahimi"
+echo "Script For Control Panel DirectAdmin"
+echo "Git: AshkanSecure | GitHub: AshkanSecure | Instagram: Secure_Manager | Email: ashkan.secure@gmail.com|"
+echo "======================================================================================================="
+
